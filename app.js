ddbbaf0112c7587fbfb5b918f887e5677b5ee0c6
Rodrigo Costa <costa.rgc@gmail.com>
2019-05-18 21:50:01 -0400

    working back end

--- app.js
+++ app.js
@@ -1,23 +1,65 @@
 // datepicker
-$(function() {
-    $( ".datepicker" ).datepicker({ dateFormat: "yyyy-mm-dd" });
-  }); 
+$(function () {
+    $(".datepicker").datepicker({ dateFormat: "yyyy-mm-dd" });
+});
 
-// start of code
+// start of code search
+
 $("#searchMe").on("click", function (event) {
     searchTerm = $('#searchterm').val().trim();
-    retrieve=$('#retrieve').val().trim();
-    startYear= $('#startYear').val().trim();
-    endYear= $('#endYear').val().trim();
-    
-    parameters = [searchTerm,retrieve,startYear,endYear]
-    
-    console.log(parameters);
+    retrieve = $('#retrieve').val().trim();
+    startYear = $('#startYear').val().trim();
+    endYear = $('#endYear').val().trim();
+
+    begin_date = "&begin_date=" + formatDate(startYear);
+    end_date = "&end_date=" + formatDate(endYear);
+
+    if (startYear == "") { begin_date = "" };
+    if (endYear == "") { end_date = "" };
+
+
+    apikey = "&api-key=" + 'bZL8pCAJYWgrasysArSqDvPTSs5MQpEd';
+
+    parameters = [searchTerm, retrieve, startYear, endYear];
+
+
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + begin_date + end_date + apikey;
+
+    console.log(queryURL);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (obj) {
+
+        console.log(obj.response.meta.hits);
+
+        for (i = 0; i < retrieve; i++) {
+            console.log(obj.response.docs[i].headline.main);
+            console.log(obj.response.docs[i].byline.original);   
+            console.log(obj.response.docs[i].section_name);
+            // console.log(obj.response.docs[i].subsection_name);
+            console.log(obj.response.docs[i].pub_date);
+            console.log(obj.response.docs[i].web_url);
+        }
+
+    });
+
 
 });
 
-searchTerm = $('#startYear').text();
-console.log(searchTerm);
+
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('');
+}
 
 // var queryURL = "https://omdbapi.com/?t=" + movieName + "&apikey=trilogy";
 
