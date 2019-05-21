c035708e88bf1ae31b4b43c4b6144f5734a4ffc0
Rodrigo Costa <costa.rgc@gmail.com>
2019-05-20 21:04:35 -0400

    bug fix rerun

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-// datepicker
-$(function () {
-    $(".datepicker").datepicker({ dateFormat: "yyyy-mm-dd" });
-});
+    // datepicker
+    $(function () {
+        $(".datepicker").datepicker({ dateFormat: "yyyy-mm-dd" });
+    });
 
-// start of code search
+    // start of code search
 
-$("#searchMe").on("click", function (event) {
-// $("#myform").on("submit", function (event) {
+    $("#searchMe").on("click", function (event) {
+        // $("#myform").on("submit", function (event) {
+        event.preventDefault();
+        // document.getElementById("myform").submit();
 
-    // document.getElementById("myform").submit();
+        searchTerm = $('#searchterm').val().trim();
+        retrieve = $('#retrieve').val().trim();
+        startYear = $('#startYear').val().trim();
+        endYear = $('#endYear').val().trim();
 
-    searchTerm = $('#searchterm').val().trim();
-    retrieve = $('#retrieve').val().trim();
-    startYear = $('#startYear').val().trim();
-    endYear = $('#endYear').val().trim();
+        begin_date = "&begin_date=" + formatDate(startYear);
+        end_date = "&end_date=" + formatDate(endYear);
 
-    begin_date = "&begin_date=" + formatDate(startYear);
-    end_date = "&end_date=" + formatDate(endYear);
-
-    if (startYear == "") { begin_date = "" };
-    if (endYear == "") { end_date = "" };
+        if (startYear == "") { begin_date = "" };
+        if (endYear == "") { end_date = "" };
 
 
-    apikey = "&api-key=" + 'bZL8pCAJYWgrasysArSqDvPTSs5MQpEd';
+        apikey = "&api-key=" + 'bZL8pCAJYWgrasysArSqDvPTSs5MQpEd';
 
-    parameters = [searchTerm, retrieve, startYear, endYear];
+        parameters = [searchTerm, retrieve, startYear, endYear];
 
 
         var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + begin_date + end_date + apikey;
 
-        console.log(queryURL);
+
 
 
 
@@ -41,11 +41,12 @@ $("#searchMe").on("click", function (event) {
         }).then(function (obj) {
             console.clear();
             console.log(obj.response.meta.hits);
+            console.log(queryURL);
 
             maxloopSearch = Math.min(retrieve, obj.response.meta.hits);
-            var results=[];
+            var results = [];
 
-
+            $('#check').remove();
             for (i = 0; i < maxloopSearch; i++) {
 
                 console.log(obj.response.docs[i].headline.main); //title_art
@@ -55,29 +56,29 @@ $("#searchMe").on("click", function (event) {
                 console.log(obj.response.docs[i].pub_date); // date_art
                 console.log(obj.response.docs[i].web_url); // link_art
 
-                results[i] = $("<div class='card mb-1'>");
+                results[i] = $("<div class='card mb-1' id=\"check\">");
                 var title = obj.response.docs[i].headline.main;
-                var pOne = $("<span id='title_art'>").text(title);
+                var pOne = $("<span class='results' id='title_art'>").text(title);
                 results[i].append(pOne);
 
                 var byline = obj.response.docs[i].byline.original;
-                var pTwo = $("<span id='by_art'>").text(byline);
+                var pTwo = $("<span  class='results' id='by_art'>").text(byline);
                 results[i].append(pTwo);
 
                 var section = obj.response.docs[i].section_name;
-                var pThree = $("<span id='sec_art'>").text(section);
+                var pThree = $("<span  class='results' id='sec_art'>").text(section);
                 results[i].append(pThree);
 
                 var pubDate = obj.response.docs[i].pub_date;
-                var pFour = $("<span id='date_art'>").text(pubDate);
+                var pFour = $("<span  class='results' id='date_art'>").text(pubDate);
                 results[i].append(pFour);
 
                 var webUrl = obj.response.docs[i].web_url;
-                var pFive = $("<a href=\""+webUrl+"\">").text(webUrl);
+                var pFive = $("<a  class='results' href=\"" + webUrl + "\">").text(webUrl);
                 results[i].append(pFive);
 
 
-            $("#results").prepend(results[i]);
+                $("#results").prepend(results[i]);
 
                 // create a div and append it to id="mainform"!... for every div create many spans with class results and append to div created.
                 //     <div class=" mb-1">
@@ -94,19 +95,19 @@ $("#searchMe").on("click", function (event) {
 
 
 
-});
+    });
 
 
-function formatDate(date) {
-    var d = new Date(date),
-        month = '' + (d.getMonth() + 1),
-        day = '' + d.getDate(),
-        year = d.getFullYear();
+    function formatDate(date) {
+        var d = new Date(date),
+            month = '' + (d.getMonth() + 1),
+            day = '' + d.getDate(),
+            year = d.getFullYear();
 
-    if (month.length < 2) month = '0' + month;
-    if (day.length < 2) day = '0' + day;
+        if (month.length < 2) month = '0' + month;
+        if (day.length < 2) day = '0' + day;
 
-    return [year, month, day].join('');
-}
+        return [year, month, day].join('');
+    }
 
 });
